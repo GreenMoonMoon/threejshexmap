de7ee7a44fd58ca9b97df2ffed8a58c74ce5e82d
GreenMoonMoon <josue.boisvert@gmail.com>
2018-08-17 18:13:50 -0400

    add coordinate system

--- src/hexamap.js
+++ src/hexamap.js
@@ -1,36 +1,46 @@
 var Hexamap;
 (function (Hexamap) {
-    const cellColor = [
+    const tempCellColor = [
         "#3a8e39",
         "#44270b",
         "#2155f2",
         "#bababa",
         "#e2d8b7"
     ];
-    class Grid {
-        constructor(width, height) {
-            this.cells = [];
-            for (let z = 0, i = 0, f = 0; z < height; z++) {
-                for (let x = 0; x < width; x++, i += 6, f += 4) {
-                    this.cells.push({
-                        coordinates: { x: x, y: -x - z, z: z },
-                        vertices: [i, i + 1, i + 2, i + 3, i + 4, i + 5],
-                        faces: [f, f + 1, f + 2, f + 3],
-                        color: new THREE.Color(cellColor[Math.floor(Math.random() * cellColor.length)])
-                    });
-                }
-            }
-        }
-    }
-    Hexamap.Grid = Grid;
+    let Direction;
+    (function (Direction) {
+        Direction[Direction["NW"] = 0] = "NW";
+        Direction[Direction["NE"] = 1] = "NE";
+        Direction[Direction["E"] = 2] = "E";
+        Direction[Direction["SE"] = 3] = "SE";
+        Direction[Direction["SW"] = 4] = "SW";
+        Direction[Direction["W"] = 5] = "W";
+    })(Direction || (Direction = {}));
+    const CubeDirectionValues = [
+        new THREE.Vector3(0, 1, -1),
+        new THREE.Vector3(1, 0, -1),
+        new THREE.Vector3(1, -1, 0),
+        new THREE.Vector3(0, -1, 1),
+        new THREE.Vector3(-1, 0, 1),
+        new THREE.Vector3(-1, 1, 0),
+    ];
+    const AxialDirectionValues = [
+        new THREE.Vector3(0, -1),
+        new THREE.Vector3(1, -1),
+        new THREE.Vector3(1, 0),
+        new THREE.Vector3(0, 1),
+        new THREE.Vector3(-1, 1),
+        new THREE.Vector3(-1, 0),
+    ];
     class Generator {
         constructor(cellSize) {
             this.outerRadius = cellSize;
             this.innerRadius = cellSize * (Math.sqrt(3) / 2);
+            this.width = cellSize * Math.sqrt(3);
             this.size = cellSize;
         }
         generate() {
-            let grid = new Grid(10, 10);
+            let grid = this.createGrid(10, 10);
             let geo = new THREE.Geometry();
             let verColor = [];
             for (let i = 0; i < grid.cells.length; i++) {
@@ -39,14 +49,28 @@ var Hexamap;
                 center.x += this.innerRadius * (cell.coordinates.z % 2);
                 geo.vertices.push(...this.getCorners(center));
                 geo.faces.push(...this.getFaces(i));
-                geo.faces[i * 4].vertexColors = [cell.color, cell.color, cell.color];
-                geo.faces[i * 4 + 1].vertexColors = [cell.color, cell.color, cell.color];
-                geo.faces[i * 4 + 2].vertexColors = [cell.color, cell.color, cell.color];
-                geo.faces[i * 4 + 3].vertexColors = [cell.color, cell.color, cell.color];
+                geo.faces[i * 4].vertexColors = [cell.tempColor, cell.tempColor, cell.tempColor];
+                geo.faces[i * 4 + 1].vertexColors = [cell.tempColor, cell.tempColor, cell.tempColor];
+                geo.faces[i * 4 + 2].vertexColors = [cell.tempColor, cell.tempColor, cell.tempColor];
+                geo.faces[i * 4 + 3].vertexColors = [cell.tempColor, cell.tempColor, cell.tempColor];
             }
             geo.computeVertexNormals();
-            let mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({ vertexColors: THREE.VertexColors }));
+            let mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({ vertexColors: THREE.VertexColors, shininess: 60 }));
             return mesh;
+        }
+        createGrid(width, height) {
+            let grid = { cells: new Array() };
+            for (let z = 0, i = 0, f = 0; z < height; z++) {
+                for (let x = 0; x < width; x++, i += 6, f += 4) {
+                    grid.cells.push({
+                        coordinates: new THREE.Vector3(x, -x - z, z),
+                        vertices: [i, i + 1, i + 2, i + 3, i + 4, i + 5],
+                        faces: [f, f + 1, f + 2, f + 3],
+                        tempColor: new THREE.Color(tempCellColor[Math.floor(Math.random() * tempCellColor.length)])
+                    });
+                }
+            }
+            return grid;
         }
         getFaces(index) {
             let minIndices = index * 6;
@@ -69,5 +93,37 @@ var Hexamap;
         }
     }
     Hexamap.Generator = Generator;
+    function CubeToAxial(coordinate) {
+        return new THREE.Vector2(coordinate.x, coordinate.z);
+    }
+    Hexamap.CubeToAxial = CubeToAxial;
+    function AxialToCube(coordinate) {
+        return new THREE.Vector3(coordinate.x, -coordinate.x - coordinate.y, coordinate.y);
+    }
+    Hexamap.AxialToCube = AxialToCube;
+    function GetCubeDirection(direction) {
+        return CubeDirectionValues[direction];
+    }
+    Hexamap.GetCubeDirection = GetCubeDirection;
+    function GetCubeDirectionFromCell(cell, direction) {
+        return cell.coordinates.add(CubeDirectionValues[direction]);
+    }
+    Hexamap.GetCubeDirectionFromCell = GetCubeDirectionFromCell;
+    function GetAxialDirection(direction) {
+        return AxialDirectionValues[direction];
+    }
+    Hexamap.GetAxialDirection = GetAxialDirection;
+    function GetAxialDirectionFromCell(cell, direction) {
+        return cell.coordinates.add(AxialDirectionValues[direction]);
+    }
+    Hexamap.GetAxialDirectionFromCell = GetAxialDirectionFromCell;
+    function DistanceCube(a, b) {
+        return (Math.abs(a.x - b.x) + Math.abs(a.y - b.y) + Math.abs(a.z - b.z)) / 2;
+    }
+    Hexamap.DistanceCube = DistanceCube;
+    function DistanceAxial(a, b) {
+        return (Math.abs(a.x - b.x), Math.abs(a.x + a.y - b.x - b.y), Math.abs(a.y - b.y)) / 2;
+    }
+    Hexamap.DistanceAxial = DistanceAxial;
 })(Hexamap || (Hexamap = {}));
 //# sourceMappingURL=hexamap.js.map
--- src/hexamap.ts
+++ src/hexamap.ts
@@ -6,35 +6,42 @@ namespace Hexamap{
         "#bababa",
         "#e2d8b7"
     ]
-
-    interface Coordinate {
-        x: number
-        y: number
-        z: number
+    
+    enum Direction {
+        NW,
+        NE,
+        E,
+        SE,
+        SW,
+        W
     }
 
+    const CubeDirectionValues = [
+        new THREE.Vector3(0, 1, -1), //THREE.Vector3 are used here since there is not distinctions between int and floats in typescript.
+        new THREE.Vector3(1, 0, -1),
+        new THREE.Vector3(1, -1, 0),
+        new THREE.Vector3(0, -1, 1),
+        new THREE.Vector3(-1, 0, 1),
+        new THREE.Vector3(-1, 1, 0),
+    ]
+
+    const AxialDirectionValues = [
+        new THREE.Vector3(0, -1),
+        new THREE.Vector3(1, -1),
+        new THREE.Vector3(1, 0),
+        new THREE.Vector3(0, 1),
+        new THREE.Vector3(-1, 1),
+        new THREE.Vector3(-1, 0),
+    ]
+
     interface Cell {
-        coordinates: Coordinate
+        coordinates: THREE.Vector3
         vertices: number[], faces: number[]
         tempColor: THREE.Color
     }
 
-    export class Grid{
-        public cells: Cell[];
-
-        public constructor(width: number, height: number){
-            this.cells = [];
-            for(let z=0, i=0, f=0; z < height; z++){
-                for(let x = 0; x < width; x++, i+=6, f+=4){
-                    this.cells.push({
-                        coordinates: {x: x, y: -x-z, z: z},
-                        vertices: [i, i+1, i+2, i+3, i+4, i+5],
-                        faces: [f, f+1, f+2, f+3],
-                        tempColor: new THREE.Color(tempCellColor[Math.floor(Math.random() * tempCellColor.length)])
-                    });
-                }
-            }
-        }
+    export interface Grid {
+        cells: Cell[];
     }
 
     export class Generator{
@@ -52,7 +59,8 @@ namespace Hexamap{
 
         public generate(): THREE.Mesh {
 
-            let grid = new Grid(10, 10);
+            let grid = this.createGrid(10, 10);
+            
             let geo = new THREE.Geometry();
             let verColor = [];
 
@@ -73,9 +81,25 @@ namespace Hexamap{
             }
 
             geo.computeVertexNormals();
-            let mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({vertexColors: THREE.VertexColors}));
+            let mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({vertexColors: THREE.VertexColors, shininess: 60}));
 
             return mesh;
+        }
+
+        private createGrid(width: number, height: number): Grid{
+
+            let grid = {cells: new Array<Cell>()};
+            for(let z=0, i=0, f=0; z < height; z++){
+                for(let x = 0; x < width; x++, i+=6, f+=4){
+                    grid.cells.push({
+                        coordinates: new THREE.Vector3(x, -x-z, z),
+                        vertices: [i, i+1, i+2, i+3, i+4, i+5],
+                        faces: [f, f+1, f+2, f+3],
+                        tempColor: new THREE.Color(tempCellColor[Math.floor(Math.random() * tempCellColor.length)])
+                    });
+                }
+            }
+            return grid;
         }
 
         private getFaces(index: number): THREE.Face3[]{
@@ -107,5 +131,37 @@ namespace Hexamap{
 
             return corners;
         }
+    }
+
+    export function CubeToAxial(coordinate: THREE.Vector3): THREE.Vector2{
+        return new THREE.Vector2(coordinate.x, coordinate.z);
+    }
+
+    export function AxialToCube(coordinate: THREE.Vector2): THREE.Vector3{
+        return new THREE.Vector3(coordinate.x, -coordinate.x-coordinate.y, coordinate.y);
+    }
+
+    export function GetCubeDirection(direction: Direction){
+        return CubeDirectionValues[direction];
+    }
+
+    export function GetCubeDirectionFromCell(cell: Cell, direction: Direction){
+        return cell.coordinates.add(CubeDirectionValues[direction]);
+    }
+
+    export function GetAxialDirection(direction: Direction){
+        return AxialDirectionValues[direction];
+    }
+
+    export function GetAxialDirectionFromCell(cell: Cell, direction: Direction){
+        return cell.coordinates.add(AxialDirectionValues[direction]);
+    }
+
+    export function DistanceCube(a: THREE.Vector3, b: THREE.Vector3){
+        return (Math.abs(a.x - b.x) + Math.abs(a.y - b.y) + Math.abs(a.z - b.z))/2;
+    }
+
+    export function DistanceAxial(a: THREE.Vector2, b: THREE.Vector2){
+        return (Math.abs(a.x - b.x), Math.abs(a.x + a.y - b.x - b.y), Math.abs(a.y - b.y))/2
     }
 }
